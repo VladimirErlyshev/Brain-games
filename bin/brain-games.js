c629758dbcc82908e3e34a8da27efa1879383a84
Dysangelium <vladimirerl@gmail.com>
2021-07-08 13:41:47 +0500

    incapsulate acquaintance

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 import acquaintance from '../src/cli.js';
 
-const welcomeMessage = 'Welcome to the Brain Games!';
-console.log(welcomeMessage);
 acquaintance();
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
+const welcomeMessage = 'Welcome to the Brain Games!';
+console.log(welcomeMessage);
 const acquaintance = () => {
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
