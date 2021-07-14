4681c17be443ecec010140a44a81099f6bd2eb82
Dysangelium <vladimirerl@gmail.com>
2021-07-14 23:08:51 +0500

    add brain-gcd

--- src/helpers/math-helper.js
+++ src/helpers/math-helper.js
@@ -1,6 +1,14 @@
 // default value is in between 0 from 100
-const getRandomValue = (range = 101) => Math.floor(Math.random() * range);
+const getRandomValue = (max = 101) => Math.floor(Math.random() * max);
 
 const isEven = (number) => number % 2 === 0;
 
-export { getRandomValue, isEven };
+const getGCD = (n, m) => {
+  if (m !== 0) {
+    const k = n % m;
+    return getGCD(m, k);
+  }
+  return n;
+};
+
+export { getRandomValue, isEven, getGCD };
