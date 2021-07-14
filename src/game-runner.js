44c8792b7c2fe1ce34125c07c446ebca5d941cdf
Dysangelium <vladimirerl@gmail.com>
2021-07-14 13:33:41 +0500

    add game-runner and refactor messages-helper

--- src/game-runner.js
+++ src/game-runner.js
@@ -1,2 +1,29 @@
+import readlineSync from 'readline-sync';
+import { showMessage } from './helpers/messages-helper';
+
 const roundsCount = 3;
 
+const gameRunner = (gameRule, gameData) => {
+  showMessage('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  showMessage(`Hello, ${userName}!`);
+  showMessage(gameRule);
+  for (let i = 0; i < roundsCount; i += 1) {
+    const [gameQuestion, correctAnswer] = gameData();
+    showMessage(`Question: ${gameQuestion}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+      showMessage('Correct!');
+    } else {
+      showMessage(
+        `${answer}  is wrong answer ;(. Correct answer was '${correctAnswer}'.`
+      );
+      showMessage(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+
+  showMessage(`Congratulations, ${userName}`);
+};
+
+export default gameRunner;
--- src/helpers/messages-helper.js
+++ src/helpers/messages-helper.js
@@ -1,19 +1,5 @@
-const showCongratulationsMessage = (name) => {
-  console.log(`Congratulations, ${name}!`);
+const showMessage = (message) => {
+  console.log(message);
 };
 
-const showTryAgainMessage = (name) => {
-  console.log(`Let's try again, ${name}!`);
-};
-
-const showWrongAnswerMessage = (incorrectAnswer, correctAnswer) => {
-  console.log(
-    `${incorrectAnswer}  is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-  );
-};
-
-export {
-  showCongratulationsMessage,
-  showWrongAnswerMessage,
-  showTryAgainMessage,
-};
+export { showMessage };
