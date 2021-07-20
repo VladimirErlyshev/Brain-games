1b90624419c6184f76bac5c5a52d6ad13c67cdae
Dysangelium <vladimirerl@gmail.com>
2021-07-20 23:34:57 +0500

    fix engine to Denislam's comments

--- src/game-runner.js
+++ src/game-runner.js
@@ -11,15 +11,13 @@ const gameRunner = (gameRule, gameData) => {
     const [gameQuestion, correctAnswer] = gameData();
     console.log(`Question: ${gameQuestion}`);
     const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (answer !== correctAnswer) {
       console.log(`${answer}  is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
-
+  console.log('Correct!');
   console.log(`Congratulations, ${userName}!`);
 };
 
