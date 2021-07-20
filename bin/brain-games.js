5b71692ffbbad359bbd8fbf9819d5ce303919870
Dysangelium <vladimirerl@gmail.com>
2021-07-20 23:40:06 +0500

    fix brain-games to Denislam's coment

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 import acquaintance from '../src/cli.js';
 
+const welcomeMessage = 'Welcome to the Brain Games!';
+console.log(welcomeMessage);
 acquaintance();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,5 @@
 import readlineSync from 'readline-sync';
 
-const welcomeMessage = 'Welcome to the Brain Games!';
-console.log(welcomeMessage);
-
 const acquaintance = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
