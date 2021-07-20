9f857be02758e4d5cd26b1f729e155f436c3a43c
Dysangelium <vladimirerl@gmail.com>
2021-07-20 23:28:05 +0500

    fix engine to Denislam's comment

--- src/game-runner.js
+++ src/game-runner.js
@@ -1,27 +1,26 @@
 import readlineSync from 'readline-sync';
-import showMessage from './helpers/messages-helper.js';
 
 const roundsCount = 3;
 
 const gameRunner = (gameRule, gameData) => {
-  showMessage('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
-  showMessage(`Hello, ${userName}!`);
-  showMessage(gameRule);
+  console.log(`Hello, ${userName}!`);
+  console.log(gameRule);
   for (let i = 0; i < roundsCount; i += 1) {
     const [gameQuestion, correctAnswer] = gameData();
-    showMessage(`Question: ${gameQuestion}`);
+    console.log(`Question: ${gameQuestion}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
-      showMessage('Correct!');
+      console.log('Correct!');
     } else {
-      showMessage(`${answer}  is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      showMessage(`Let's try again, ${userName}!`);
+      console.log(`${answer}  is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
       return;
     }
   }
 
-  showMessage(`Congratulations, ${userName}!`);
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default gameRunner;
