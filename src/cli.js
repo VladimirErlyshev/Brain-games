76fb5a71b6cc811cbe6833c61dbd6729329b06a6
Dysangelium <vladimirerl@gmail.com>
2021-07-20 23:42:17 +0500

    fix brain-games to Denislam's coment

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const acquaintance = () => {
+  const welcomeMessage = 'Welcome to the Brain Games!';
+  console.log(welcomeMessage);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
