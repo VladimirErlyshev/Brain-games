cd86ac29e17e3e37093c896dd1a815405e2de5b8
Dysangelium <vladimirerl@gmail.com>
2021-07-26 10:51:10 +0500

    refactor brain-progression-game

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -3,23 +3,23 @@ import { getRandomValue } from '../helpers/math-helper.js';
 
 const gameRule = 'What number is missing in the progression?';
 
-const generateProgression = () => {
-  let value = getRandomValue(0, 100);
-  const progressionStep = getRandomValue(1, 10);
+const generateProgression = (startElement, progressionStep, hiddenIndex) => {
+  let value = startElement;
   const progression = [];
   for (let i = 0; i < 10; i += 1) {
     progression.push(value);
     value += progressionStep;
   }
-
+  progression[hiddenIndex] = '..';
   return progression;
 };
 
 const gameData = () => {
-  const progression = generateProgression();
-  const hiddenIndex = getRandomValue(0, progression.length - 1);
-  const correctAnswer = progression[hiddenIndex];
-  progression[hiddenIndex] = '..';
+  const startElement = getRandomValue(0, 100);
+  const progressionStep = getRandomValue(1, 10);
+  const hiddenIndex = getRandomValue(0, 10);
+  const progression = generateProgression(startElement, progressionStep, hiddenIndex);
+  const correctAnswer = startElement + progressionStep * hiddenIndex;
   const gameQuestion = progression.join(' ');
 
   return [gameQuestion, String(correctAnswer)];
